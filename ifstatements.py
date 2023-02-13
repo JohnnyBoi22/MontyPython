d63efb5428add08e3cd7c468f9ad94e72081081a
Brandon Murry <murry.brandon@fcboe.org>
2023-02-13 11:08:14 -0500

    Fixed indentation

--- ifstatements.py
+++ ifstatements.py
@@ -3,14 +3,14 @@ b = 200
 if b > a:
   print("b is greater than a")
   
- c = 33
+c = 33
 d = 33
 if c > d:
   print("c is greater than d")
 elif c == d:
   print("c and d are equal")
   
-  e = 200
+e = 200
 f = 33
 if e > f:
   print("e is greater than f")
