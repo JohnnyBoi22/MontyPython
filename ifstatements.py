a85c07d598846fcfe9c6d2487175e2456b4a9dd4
John Windham <110920206+JohnnyBoi22@users.noreply.github.com>
2023-02-13 11:01:39 -0500

    Create ifstatements.py

---
+++ ifstatements.py
@@ -0,0 +1,21 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  
+ c = 33
+d = 33
+if c > d:
+  print("c is greater than d")
+elif c == d:
+  print("c and d are equal")
+  
+  e = 200
+f = 33
+if e > f:
+  print("e is greater than f")
+elif e == f:
+  print("e and f are equal")
+else:
+  print("e is greater than f")
+  
